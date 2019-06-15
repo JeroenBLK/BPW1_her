5767604d8c2231568e5af727bd078d1dde0c0e44
JeroenBLK <jeroenblk@hotmail.com>
2019-06-15 04:23:22 +0200

    Final push (hopefullu)

---
+++ Gun test/Assets/scripts/CowntdownTimer.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class CowntdownTimer : MonoBehaviour
+{
+    float currentTime = 0f;
+    float startingTime = 30f;
+    AudioSource myAudio;
+
+    [SerializeField] Text countdownText;
+
+    private void Start()
+    {
+        currentTime = startingTime;
+
+        myAudio = GetComponent<AudioSource>();
+        myAudio.PlayDelayed(19.0f);
+    }
+
+    private void Update()
+    {
+        currentTime -= 1 * Time.deltaTime;
+        countdownText.text = currentTime.ToString ("0");
+
+        if(currentTime <= 0)
+        {
+            SceneManager.LoadScene("MainMenu");
+        }
+
+        if(currentTime <= 10)
+        {
+            countdownText.color = Color.red;
+            
+        }
+    }
+
+}
---
+++ Gun test/Assets/scripts/gunscript.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+public class gunscript : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 100f;
+
+    public Camera fpsCam;
+    public ParticleSystem muzzleFlash;
+    public GameObject impactEffect;
+
+    private AudioSource mAudioSrc;
+
+    public Rigidbody rb;
+
+    void Start()
+    {
+        mAudioSrc = GetComponent<AudioSource>();
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+        
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+            mAudioSrc.Play();
+        }
+
+    }
+
+    void Shoot()
+    {
+        muzzleFlash.Play();
+
+        /*RaycastHit hit;
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+
+            Target target = hit.transform.GetComponent<Target>();
+            if (target != null)
+            {
+                target.TakeDamage(damage);
+            }
+
+            GameObject ImpactGO = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(ImpactGO, 2f);/
+        }
+        */
+    }
+}
